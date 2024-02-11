e846bb61f5f95b068fbd21f4946c140d9ac90173
Bartosz Jagiełło <bartoszjagiello@wp.pl>
2024-02-11 22:07:44 +0100

    Coś się zmieniło chyba

---
+++ change.js
@@ -0,0 +1,29 @@
+function value(){
+    document.getElementById("t1").innerHTML = "Lesson " + parseInt(1 + u)+"<br>" + content[1 + u];
+    document.getElementById("t2").innerHTML = "Lesson " + parseInt(2 + u)+"<br>" + content[2 + u];
+    document.getElementById("t3").innerHTML = "Lesson " + parseInt(3 + u)+"<br>" + content[3 + u];
+    document.getElementById("t4").innerHTML = "Lesson " + parseInt(4 + u)+"<br>" + content[4 + u];
+}
+let u = 0;
+let content = ["nic tu nie ma","Installing Python","The fundamentals of python"," Data types, input/output information, operators","Basic Variables"]
+value();
+
+function goNext() {
+    u += 4;
+    value();
+}
+function goBack() {
+    if (u > 0) {
+        u -= 4;
+        value();
+    }
+}
+
+function set(x){
+    let z = parseInt(x+u);
+    setPage(z);
+}
+
+function goSand(){
+    window.open("test.html", "_blank");
+}
---
+++ change_page.js
@@ -0,0 +1,43 @@
+
+function back() {
+    if (page > 1) {
+        page--;
+        document.getElementById("c").innerHTML = "<embed src='l" + page + ".html'>";
+        showLesson();
+    }
+}
+
+function next() {
+    if (page < 8) {
+        page++;
+        document.getElementById("c").innerHTML = "<embed src='l" + page + ".html'>";
+        showLesson();
+    }
+}
+
+function showLesson() {
+
+    document.getElementById("unit_p").innerHTML = "Lesson " + page;
+}
+
+function setPage(p) {
+    p = parseInt(p);
+    window.location = "learn.html?" + p;
+}
+
+
+let page = window.location.search.split("?")[1];
+document.getElementById("c").innerHTML = "<embed src='l" + page + ".html'>";
+showLesson();
+function goVSC() {
+    window.open("https://code.visualstudio.com/", "_blank");
+}
+
+function goPYcharm() {
+    window.open("https://www.jetbrains.com/pycharm/", "_blank");
+}
+
+function goPY() {
+    window.open("https://www.python.org/downloads/", "_blank");
+}
+
